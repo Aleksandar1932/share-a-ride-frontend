1c2a52d65c37f6b21f1dc38b3fadbfbd95366fc4
Aleksandar Ivanovski <aleksandar.ivanovski123@gmail.com>
2021-07-15 11:40:06 +0200

    Update offer.tsx

--- pages/home/offer.tsx
+++ pages/home/offer.tsx
@@ -275,7 +275,7 @@ const Offer: React.FC = () => {
                       <Text>Price: {trip["price"]}</Text>
                       <Text>Offered Seats: {trip["offeredSeats"]}</Text>
                       <Text>Driver: {trip["driver"]["username"]}</Text>
-                      <Link>Meeting Place</Link>
+                      <Link href={"#"}>Meeting Place</Link>
                     </Box>
                     <Spacer></Spacer>
                     <Box alignItems="right">
